120b78ff901b1b91d2ffd78f3925b425f23f49b9
1Mujtaba1 <mujtabacreations@gmail.com>
2026-07-26 10:33:40 +0530

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-white">
+        CodeNova
+      </h1>
+    </main>
+  );
+}
+
+export default App;
